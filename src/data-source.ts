7762500f82c1c0b89717a4231a81d07e0538a9c3
unknown <robertcrossland@protonmail.com>
2022-12-23 18:09:15 -0600

    Revert "feat: updated the way the routing works"
    This reverts commit 25a458a05aaf1e631ddd0da1f44f90f7ae0516c0.

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,10 +2,11 @@ import { DataSource } from "typeorm";
 import { Book } from "./entity/Book";
 import { Chapter } from "./entity/Chapter";
 import { Verse } from "./entity/Verse";
+import { root } from "./paths";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: `../data/kjv.db`,
+  database: `${root}/data/kjv.db`,
   entities: [Book, Chapter, Verse],
   logging: true,
   synchronize: true,
